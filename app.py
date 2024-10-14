f243cc30d2b1cf4b571efbf93b20cc4eaf77f44f
David F Watson <davidfwatson@gmail.com>
2024-10-13 21:01:22 -0700

    address missing impls

--- app.py
+++ app.py
@@ -125,10 +125,51 @@ def admin(event_id):
     return render_template('admin.html', event=event_config, rsvps=rsvps)
 
 def generate_email_body(event, rsvp):
-    # ... (same as before)
+    if rsvp['attending'] == 'yes':
+        body = f"""
+# Thank you for your RSVP, {rsvp['name']}!
+
+We're excited that you'll be joining us for **{event['name']}**!
+
+## Event Details:
+- **Date:** {event['date']} at {event['time']}
+- **Location:** {event['location']}
+
+{event['description']}
+
+We have you down for **{rsvp['num_guests']}** guest(s) total.
+
+If you need to make any changes to your RSVP, please contact us.
+
+We look forward to seeing you there!
+"""
+    else:
+        body = f"""
+# Thank you for your RSVP, {rsvp['name']}
+
+We're sorry to hear that you won't be able to join us for **{event['name']}**, but we appreciate you letting us know.
+
+If your plans change and you're able to attend, please let us know.
+
+We hope to see you another time!
+"""
+    return body
 
 def generate_invitation_email_body(event):
-    # ... (same as before)
+    return f"""
+You're invited to {event['name']}!
+
+Join us for a special event:
+
+Date: {event['date']} at {event['time']}
+Location: {event['location']}
+
+{event['description']}
+
+Please RSVP by visiting: {url_for('index', event_id=event['id'], _external=True)}
+
+We hope to see you there!
+"""
 
 if __name__ == '__main__':
     app.run(debug=True)

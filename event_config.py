6f262ac9f378eb66f6d5c1dc8406bc859d43de43
David Watson <davidfwatson@gmail.com>
2024-10-13 22:08:28 -0700

    domain aliases

--- event_config.py
+++ event_config.py
@@ -12,8 +12,17 @@ events = {
 }
 
 
+# Mapping of alternative domains to main domains
+domain_aliases = {
+    "127.0.0.1:5000": "test.davidfwatson.com",
+    # Add more aliases as needed, e.g.:
+    # "localhost:5000": "test.davidfwatson.com",
+}
+
 def get_event_config(domain):
-    return events.get(domain, {})
+    # Check if the domain is an alias, and if so, use the main domain
+    main_domain = domain_aliases.get(domain, domain)
+    return events.get(main_domain, {})
 
 def get_all_events():
     return events

a5e6a634f624fad4f000761044f9b29c6de72d98
David Watson <davidfwatson@gmail.com>
2025-01-24 22:40:04 -0800

    update spacing

--- email_content.py
+++ email_content.py
@@ -1,16 +1,17 @@
 from flask import url_for
 
+
 def generate_confirmation_email_body(event, rsvp):
-    if rsvp['attending'] == 'yes':
-        guest_details = f"{rsvp['num_adults']} adult{'s' if rsvp['num_adults'] != 1 else ''}"
-        if rsvp['num_children'] > 0:
-            guest_details += f" and {rsvp['num_children']} child{'ren' if rsvp['num_children'] != 1 else ''}"
-            
-        dietary_info = ""
-        if rsvp.get('dietary_restrictions'):
-            dietary_info = f"\n\nYou've noted the following dietary restrictions:\n{rsvp['dietary_restrictions']}"
-            
-        body = f"""
+  if rsvp['attending'] == 'yes':
+    guest_details = f"{rsvp['num_adults']} adult{'s' if rsvp['num_adults'] != 1 else ''}"
+    if rsvp['num_children'] > 0:
+      guest_details += f" and {rsvp['num_children']} child{'ren' if rsvp['num_children'] != 1 else ''}"
+
+    dietary_info = ""
+    if rsvp.get('dietary_restrictions'):
+      dietary_info = f"\n\nYou've noted the following dietary restrictions:\n{rsvp['dietary_restrictions']}"
+
+    body = f"""
 # Thank you for your RSVP, {rsvp['name']}!
 
 We're excited that you'll be joining us for **{event['name']}**!
@@ -27,8 +28,8 @@ If you need to make any changes to your RSVP, please contact us.
 
 We look forward to seeing you there!
 """
-    else:
-        body = f"""
+  else:
+    body = f"""
 # Thank you for your RSVP, {rsvp['name']}!
 
 We're sorry you won't be able to join us for **{event['name']}**, but we appreciate you letting us know.
@@ -37,5 +38,5 @@ If your plans change and you'd like to attend, please contact us.
 
 We hope to see you another time!
 """
-    
-    return body
+
+  return body
